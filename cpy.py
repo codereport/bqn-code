105f0626a376a5671a8500d3c69a0ef19bc98918
Conor Hoekstra <codereport@outlook.com>
2024-07-15 13:10:03 -0400

    :sparkles: add aoc/2022 copying

--- cpy.py
+++ cpy.py
@@ -10,26 +10,30 @@ def zero_pad(n: int) -> str:
 def target(type: str, N: int) -> str:
     match type:
         case "pwc":      return f"../perlweeklychallenge-club/challenge-{N}/conor-hoekstra"
+        case "aoc/2022": return f"../Advent-of-Code-2022/"
         case "aoc/2023": return f"../Advent-of-Code-2023/"
         case "leetcode": return f"../leetcode/"
 
 def source_file_name(type: str, N: int, i: int) -> str:
     match type:
         case "pwc":      f = f"{N}-{i}.bqn"            # 273-1.bqn
-        case "aoc/2023": f = f"day{zero_pad(N)}.bqn"   # day01.bqn
+        case "aoc/2022" \
+           | "aoc/2023": f = f"day{zero_pad(N)}.bqn"   # day01.bqn
         case "leetcode": f = f"{N // 10}-{N % 10}.bqn" # 403-1.bqn
     return f"{type}/{f}"
 
 def dest_file_name(type: str, i: int) -> str:
     match type:
         case "pwc":      return f"ch-{i}.bqn"                      # ch-1.bqn
-        case "aoc/2023": return f"day{zero_pad(i)}.bqn"            # day01.bqn
+        case "aoc/2022" \
+           | "aoc/2023": return f"day{zero_pad(i)}.bqn"            # day01.bqn
         case "leetcode": return f"0{N // 10}_Problem_{N % 10}.bqn" # 0403_Problem_1.bqn
 
 def file_numbers(type: str, n: int):
     match type:
         case "pwc":      return [1, 2]
-        case "aoc/2023": return [n]
+        case "aoc/2022" \
+           | "aoc/2023": return [n]
         case "leetcode": return [n]
 
 def copy_and_modify_files(N: int, type: str):
@@ -64,7 +68,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: python script.py type N")
     else:
-        valid_types = ["pwc", "aoc/2023", "leetcode"]
+        valid_types = ["pwc", "aoc/2022", "aoc/2023", "leetcode"]
         type = sys.argv[1]
         if type not in valid_types:
             print(f"type must be in {valid_types}")
